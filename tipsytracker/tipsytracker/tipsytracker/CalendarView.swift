d9dbc3a337e99bbb067b6e494d3dcbfaeb97aa0e
Lauren Jones <77810213+laurennnjones@users.noreply.github.com>
2022-04-19 01:19:15 -0400

    Add files via upload

---
+++ tipsytracker/tipsytracker/tipsytracker/CalendarView.swift
@@ -0,0 +1,21 @@
+//
+//  CalendarView.swift
+//  tipsytracker
+//
+//  Created by Lauren Jones on 4/18/22.
+//
+
+import SwiftUI
+
+struct CalendarView: View {
+    var body: some View {
+        // https://www.youtube.com/watch?v=UZI2dvLoPr8
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CalendarView_Previews: PreviewProvider {
+    static var previews: some View {
+        CalendarView()
+    }
+}
---
+++ tipsytracker/tipsytracker/tipsytracker/NavigationView.swift
@@ -0,0 +1,51 @@
+//
+//  NavigationBar.swift
+//  tipsytracker
+//
+//  Created by Lauren Jones on 4/18/22.
+//
+
+import SwiftUI
+
+struct NavigationView: View {
+    var body: some View {
+        HStack{
+            Button(action: {
+                // add calendar view
+                }, label: {
+                    Image(systemName: "calendar.badge.plus")
+                        .colorInvert()
+                })
+            .padding()
+            Spacer()
+            Image("Logo")
+                .resizable()
+                .frame(width: 125.0, height: 80.0)
+            Spacer()
+            Button(action: {
+                    // action should lead to settings
+                    // settings will include emergency contact
+                }, label: {
+                    Image(systemName: "gear")
+                        .colorInvert()
+            })
+            .padding()
+        }
+            
+        VStack {
+            Text("Tipsy Tracker")
+                .font(.title)
+                .fontWeight(.semibold)
+            Text("What Are You Drinking Tonight?")
+                .font(.subheadline)
+                .fontWeight(.semibold)
+        }
+        Spacer()
+    }
+}
+
+struct NavigationView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationView()
+    }
+}
